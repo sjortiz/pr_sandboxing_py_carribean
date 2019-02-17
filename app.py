5f070192167508e8288078de6c3372c44a2dbe13
ShailynOrtiz <sortiz@intellisys.com.do>
2019-02-16 23:36:32 -0400

    adding kill containers methods

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 import requests
 
@@ -6,6 +7,72 @@ from queries.prs import query_prs_from_user
 
 
 Token = os.getenv('PR_SANDBOXING_TOKEN')
+
+__open = []
+__killed = []
+
+
+def kill_container(pr_name):
+
+    if not pr_name in __killed:
+
+        print(f'Attempting to kill pr {pr_name}')
+
+        folder_address = f'pulls/{pr_name}'
+
+        if path.exists(folder_address):
+
+            print('PR Found!')
+
+            print('killing')
+
+            sp = subprocess.Popen(
+                [
+                    'docker-compose',
+                    'down', '--remove-orphans'
+                ],
+                stdout=subprocess.PIPE,
+                cwd=folder_address,
+            )
+
+        __killed = pr_name
+
+
+def create_container(pr_name):
+
+    if not pr_name in __open:
+        print(pr_name, 'creating')
+
+
+def iterate(name, pr_list, func):
+
+    for pr in pr_list:
+
+        compounded_name = f'{pr}-{name}'
+        func(compounded_name)
+
+
+def kill_containers(repo_name, prs):
+    iterate(repo_name, prs, kill_container)
+
+
+def create_containers(repo_name, prs):
+    iterate(repo_name, prs, create_container)
+
+
+def parse_pr_numbers(prs: list) -> list:
+    return [
+        pr.get('number')
+        for pr in prs
+    ]
+
+
+def split_data(repo: list) -> [list, list, list]:
+    return (
+        parse_pr_numbers(repo.get('open', {}).get('nodes', [])),
+        parse_pr_numbers(repo.get('merged', {}).get('nodes', [])),
+        parse_pr_numbers(repo.get('closed', {}).get('nodes', [])),
+    )
 
 
 if __name__ == '__main__':
@@ -32,12 +99,11 @@ if __name__ == '__main__':
 
     unparsed_repos = info.get('nodes')
 
-    to_kill = []
-    to_create = []
-    to_maintain = []
-
     for repo in unparsed_repos:
 
-        _open = repo.get('open', {}).get('nodes', [])
-        _merged = repo.get('merged', {}).get('nodes', [])
-        _closed = repo.get('closed', {}).get('nodes', [])
+        _name = repo.get('name', '')
+        _open, _merged, _closed = split_data(repo)
+        _finish_status = _merged + _closed
+
+        create_containers(_name, _open)
+        kill_containers(_name, _finish_status)
