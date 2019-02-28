2743ea09bd177213c656c8a5d6f9e291e28b7881
ShailynOrtiz <sortiz@intellisys.com.do>
2019-02-28 12:17:24 -0400

    extending query to retrieve the last commit id, creating handlers module to convert the data to git and docker custom objects (objects defined by me not by the libraries, fixing config.yaml

--- sandboxit/handlers/__init__.py
+++ sandboxit/handlers/__init__.py
@@ -1,10 +1,14 @@
+"""Module to transform plain data in objects with methods"""
+
+# Built-in imports
 import datetime
 
 
 class Git:
+    """Reshape and manage all the git related processes"""
 
     def __init__(self, data: list) -> None:
-
+        """Receives the data and and fill the Git object"""
         self.__ssh_url = data.get('sshUrl')
         self.__prs = data.get('pullRequests', {}).get('nodes', [])
         self.__prs = Git.__re_structure_prs(self.__prs)
@@ -12,32 +16,53 @@ class Git:
 
     @staticmethod
     def __re_structure_prs(prs: list) -> dict:
-
-        __prs = {}
+        """Group the prs by stage into a dictionary
+        e.g:
+        [
+            {"id": 1, "state": "MERGED"}, ...,
+            {"id": 90, "state": "OPEN"}, ...
+        ]
+        returns
+        {
+            'MERGED': [{"id": 1, "state": "MERGED"}, ...],
+            'OPEN': [{"id": 90, "state": "OPEN"}, ...]
+        }
+        """
+        __prs = {
+            'MERGED': [],
+            'OPEN': [],
+            'CLOSED': [],
+        }
 
         for pr in prs:
-
-            state = pr.get('state')
-
-            if state in __prs:
-                __prs[state].append(pr)
-
-            else:
-                __prs[state] = [pr]
+            __prs[pr.get('state')].append(pr)
 
         return __prs
 
-    def pull(self):
 
-        if self.__prs.get('OPEN'):
+    def clone(self):
+        """Clone repo into local folder
+        TODO:
+            - Logic to clone
+        """
+
+        __open_prs = self.__prs.get('OPEN')
+
+        if __open_prs:
 
             if not self.__fetched:
 
                 print('Pulling repository')
                 self.__fetched = datetime.datetime.now().time()
 
-            else:
-                print('Pulling updates if any')
+
+    def pull(self): ...
+        """Updates the branch if there is any changes in the PR
+        TODO:
+            - Comparision with last commit id
+            - Checkout and pull branch into new folder
+        """
+
 
 
 class Docker:
